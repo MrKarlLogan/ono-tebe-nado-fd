e44ec9aa59017ffe323005db6e16c1575e67917d
Centurion <mrelectricguitar7@gmail.com>
2025-02-20 21:25:40 +0300

    Финальная работа

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/MrKarlLogan/ono-tebe-nado-fd
---
+++ script.js
@@ -0,0 +1,5 @@
+function bid() {
+  let userbid = prompt("Сделайте Вашу ставку")
+  return alert("Ваша ставка: " + userbid + " рублей")
+};
+
